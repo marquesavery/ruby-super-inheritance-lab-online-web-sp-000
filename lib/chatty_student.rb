6c8e17edc92e1e6694ae977f60acf14390be6077
Marques Avery <averymarques4@gmail.com>
2020-08-29 20:27:34 +0000

    Automatically backed up by Learn

---
+++ lib/chatty_student.rb
@@ -0,0 +1,12 @@
+require "./student.rb"
+
+class ChattyStudent
+
+  def hello
+    super
+    put "How are you doing today? I'm okay, but
+    I'm kind of tired. Did you watch The Walking
+    Dead last night? You didn't?! Oh man, it was so crazy!
+    What, you don't want any spoilers? Okay well let me just tell you who died..."
+
+end
